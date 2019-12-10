598bc0c13a119c35bfe6b6d6162ba8abb2052a37
Bruce Smith <jbsmith@smithcasa.net>
2019-12-10 08:54:33 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-mdx',
+    {
+        resolve: `gatsby-plugin-sass`,
+        options: {
+            includePaths: ['./node_modules/'],
+        },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,6 +22,13 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            name: 'components',
+            path: './src/components',
+        },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,6 +41,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-react-docgen`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/app/layout"
+import Image from "../components/app/image"
+import SEO from "../components/app/seo"
 
 const IndexPage = () => (
   <Layout>
